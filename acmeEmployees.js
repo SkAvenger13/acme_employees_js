c2b12548883dba699049265baadcbeaf4b8aeb82
Tom Warner <tommwarner@gmail.com>
2023-04-25 20:42:27 -0400

    fixed some of acmeEmployees.js

--- acmeEmployees.js
+++ acmeEmployees.js
@@ -17,7 +17,53 @@ const spacer = (text)=> {
   console.log(`${stars} ${text} ${stars}`);
 }
 
-spacer('findEmployeeByName Moe')
+const findEmployeeByName = (inputName, inputArr) => {
+    let result = {};
+    inputArr.forEach((object) => {
+        if (object.name === inputName) {
+            result = object;
+        }
+    });
+    return result;
+}
 
-console.log(findEmployeeByName('moe', employees));
-spacer('')
+const findManagerFor = (employeeObj, inputArr) => {
+    let result = {};
+    inputArr.forEach((object) => {
+        if (object.id === employeeObj.managerId) {
+            result = object;
+        }
+    });
+    return result;
+}
+
+const findCoworkersFor = (employeeObj, inputArr) => {
+    let result = [];
+    inputArr.forEach((object) => {
+        if (object.managerId === employeeObj.managerId && object !== employeeObj) {
+            result.push(object);
+        }
+    });
+    return result;
+}
+
+const findManagementChain = (employeeObj, inputArr) => {
+    let result = [];
+    inputArr.forEach((object) => {
+        if () {
+
+        }
+    });
+    return result;
+}
+
+spacer('');
+
+spacer('findManagementChain for moe')
+
+console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));
+spacer('');
+
+spacer('findManagementChain for shep Jr.')
+console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));
+spacer('');
